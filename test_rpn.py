49dc9590d2e4a25e596a2a80f6aaab2d30e1d771
shanesms <shanesms@umich.edu>
2017-03-23 22:05:15 -0400

    increase coverage

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,22 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-	def test_subtract(self):
+	def test_exponent(self):
 		result = rpn.calculate('5 2 ^')
 		self.assertEqual(25, result)
+	def test_multiply(self):
+		result = rpn.calculate('1 1 *')
+		self.assertEqual(1, result)
+		result = rpn.calculate('1 2 *')
+		self.assertEqual(2, result)
+		result = rpn.calculate('5 6 *')
+		self.assertEqual(30, result)
+		result = rpn.calculate('6 5 *')
+		self.assertEqual(30, result)
+	def test_divide(self):
+		result = rpn.calculate('30 5 /')
+		self.assertEqual(6, result)
+		result = rpn.calculate('5 5 /')
+		self.assertEqual(1, result)
+		result = rpn.calculate('0 9 /')
+		self.assertEqual(0, result)
